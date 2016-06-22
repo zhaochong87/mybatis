06abf4fca5c138750d4359357f7288e9962708fb
zhaochong87@git <zhaochong87@honeykids.com.cn>
2016-06-22 11:24:08 +0800

    mybatis实战教程mybatis in action之二：以接口的方式编程
    简单查询：映射接口

--- mybatis-2/src/test/java/com/zhaochong/mybatis/UserTest.java
+++ mybatis-2/src/test/java/com/zhaochong/mybatis/UserTest.java
@@ -8,7 +8,6 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import java.io.Reader;
-import java.util.List;
 
 
 /**
@@ -36,21 +35,6 @@ public class UserTest {
 	}
 
 	/**
-	 * getUserById 映射实体
-	 */
-	public void getUserById() {
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			// 映射实体
-			User user = (User) session.selectOne("com.zhaochong.mybatis.bean.UserMapper.selectUserByID", 1);
-			System.out.println(user.getUserAddress());
-			System.out.println(user.getUserName());
-		} finally {
-			session.close();
-		}
-	}
-
-	/**
 	 * getUserById 映射接口
 	 */
 	public void getUserById2() {
@@ -66,106 +50,10 @@ public class UserTest {
 		}
 	}
 
-	/**
-	 * 添加用户
-	 */
-	public void addUser() {
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			// 用户对象
-			User user = new User();
-			user.setUserAddress("人民广场");
-			user.setUserName("飞鸟");
-			user.setUserAge(80);
-
-			// 添加用戶
-			UserDao userDao = session.getMapper(UserDao.class);
-			userDao.addUser(user);
-
-			// 提交事务
-			session.commit();
-
-			System.out.println("当前增加的用户 id为:" + user.getId());
-		} finally {
-			session.close();
-		}
-	}
-
-	/**
-	 * 查询用户列表
-	 *
-	 * @param userName
-	 *            用户名
-	 */
-	public void getUserList(String userName) {
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			// 映射接口
-			UserDao userDao = session.getMapper(UserDao.class);
-			List<User> users = userDao.selectUsers(userName);
-			for (User user : users) {
-				System.out.println(user.getId() + ":" + user.getUserName() + ":" + user.getUserAddress());
-			}
-		} finally {
-			session.close();
-		}
-	}
-
-	/**
-	 * 更新用户
-	 */
-	public void updateUser() {
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			// 更新用戶
-			UserDao userDao = session.getMapper(UserDao.class);
-
-			User user = userDao.selectUserByID(2);
-			user.setUserAddress("xxxxx");
-			user.setUserName("yy");
-			user.setUserAge(80);
-
-			userDao.updateUser(user);
-
-			// 提交事务
-			session.commit();
-
-			// 显示更新后的信息
-			System.out.println(userDao.selectUsers(user.getUserName()));
-		} finally {
-			session.close();
-		}
-	}
-
-	/**
-	 * 删除用户
-	 */
-	public void deleteUser() {
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			// 删除用戶
-			UserDao userDao = session.getMapper(UserDao.class);
-			userDao.deleteUser(4);
-
-			// 提交事务
-			session.commit();
-
-			// 显示更新后的信息
-			userDao.selectUsers("%");
-		} finally {
-			session.close();
-		}
-	}
-
 	public static void main(String[] args) {
 		UserTest userTest = new UserTest();
 
-		// userTest.getUserById();
-//		userTest.getUserById2();
-		userTest.addUser();
-		userTest.getUserList("%");
-//		userTest.updateUser();
-		// userTest.deleteUser();
+		userTest.getUserById2();
 
 	}
 
